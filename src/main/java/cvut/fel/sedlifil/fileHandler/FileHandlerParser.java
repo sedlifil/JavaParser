958b55bae24c6323b7633bb6ffe42af8c9c5aac4
Filip Sedlisky <sedlifil@fel.cvut.cz>
2018-03-19 14:26:19 +0100

    zmena artifialID v pom.xml pro kazdy block

--- src/main/java/cvut/fel/sedlifil/fileHandler/FileHandlerParser.java
+++ src/main/java/cvut/fel/sedlifil/fileHandler/FileHandlerParser.java
@@ -2,7 +2,10 @@ package cvut.fel.sedlifil.fileHandler;
 
 import com.github.javaparser.ast.CompilationUnit;
 import cvut.fel.sedlifil.parser.ParserClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -17,59 +20,67 @@ import java.util.Map;
 /**
  * Created by filip on 09.12.17.
  */
-public class FileHandlerParser implements IFileHandlerParser{
+public class FileHandlerParser implements IFileHandlerParser {
     private String locationOfDirectory;
     private static final String DATE_PATTERN = "yyyy_MM_dd_HH-mm-ss";
     private static final String APP_NONAME = "unKnown";
+    private Logger logger;
+
 
     public FileHandlerParser(String locationOfDirectory) {
         this.locationOfDirectory = Paths.get(locationOfDirectory).toAbsolutePath().toString();
+        logger = LoggerFactory.getLogger(FileHandlerParser.class);
+
     }
 
     public FileHandlerParser() {
         locationOfDirectory = FileHandlerParser.class.getProtectionDomain().getCodeSource().getLocation().getPath();
-        locationOfDirectory = locationOfDirectory.substring(0, locationOfDirectory.indexOf(ParserClass.JAVA_TARGET)-1);
+        locationOfDirectory = locationOfDirectory.substring(0, locationOfDirectory.indexOf(ParserClass.JAVA_TARGET) - 1);
+        logger = LoggerFactory.getLogger(FileHandlerParser.class);
+
     }
 
     @Override
     public void saveAppToFile(Map<String, CompilationUnit> listBlock1,
                               Map<String, CompilationUnit> listBlock2,
                               Map<String, CompilationUnit> listBlock3,
-                              List<String> listAllBlock){
+                              List<String> listAllBlock) {
+        logger.info("starts to saving app into blocks...");
         DateFormat dateFormat = new SimpleDateFormat(DATE_PATTERN);
         Date date = new Date();
 
         String AppName = APP_NONAME;
-        if(listBlock1.entrySet().stream().findFirst().isPresent()){
+        if (listBlock1.entrySet().stream().findFirst().isPresent()) {
             AppName = getAppNameFromPath(listBlock1.entrySet().stream().findFirst().get().getKey());
-        }else if(listBlock2.entrySet().stream().findFirst().isPresent()){
+        } else if (listBlock2.entrySet().stream().findFirst().isPresent()) {
             AppName = getAppNameFromPath(listBlock2.entrySet().stream().findFirst().get().getKey());
-        }else if(listBlock3.entrySet().stream().findFirst().isPresent()){
+        } else if (listBlock3.entrySet().stream().findFirst().isPresent()) {
             AppName = getAppNameFromPath(listBlock3.entrySet().stream().findFirst().get().getKey());
         }
 
-        if (AppName.equals(APP_NONAME)){
+        if (AppName.equals(APP_NONAME)) {
             return;
         }
 
         String directoryOfApp = locationOfDirectory + AppName + dateFormat.format(date);
+        logger.info("generated app is located in " + directoryOfApp + ".");
 
         if (!Files.exists(Paths.get(directoryOfApp))) {
             try {
                 Files.createDirectories(Paths.get(directoryOfApp));
-                //System.out.println("Directory created!");
             } catch (IOException e) {
-                e.printStackTrace();
+                logger.error("Error with creating directory!!!");
+                logger.error("FileHandler exits with error!!!");
                 return;
             }
         }
         saveBlockAppToFile(listBlock1, directoryOfApp, ParserClass.BLOCK1_);
         saveBlockAppToFile(listBlock2, directoryOfApp, ParserClass.BLOCK2_);
         saveBlockAppToFile(listBlock3, directoryOfApp, ParserClass.BLOCK3_);
-        saveToAllBlocksAppToFile(listAllBlock, directoryOfApp, AppName);
+        saveAllBlocksAppToFile(listAllBlock, directoryOfApp, AppName);
     }
 
-    private void saveBlockAppToFile(Map<String, CompilationUnit> blockList, String directoryOfApp, String blockName){
+    private void saveBlockAppToFile(Map<String, CompilationUnit> blockList, String directoryOfApp, String blockName) {
         blockList.forEach((class_, cu) -> {
             String class_Path = class_.substring(class_.indexOf(ParserClass.JAVA_SOURCE), class_.lastIndexOf(ParserClass.FILE_DELIMITER));
             String class_PathWithName = class_.substring(class_.indexOf(ParserClass.JAVA_SOURCE), class_.length());
@@ -77,54 +88,48 @@ public class FileHandlerParser implements IFileHandlerParser{
                 Path fileName = Paths.get(directoryOfApp + ParserClass.FILE_DELIMITER + blockName + class_PathWithName);
                 Files.createDirectories(Paths.get(directoryOfApp + ParserClass.FILE_DELIMITER + blockName + class_Path));
                 Files.createFile(fileName);
-                Files.write(fileName, cu.toString().getBytes(StandardCharsets.UTF_8)); // Java 7+ only);
+                Files.write(fileName, cu.toString().getBytes(StandardCharsets.UTF_8));
 
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            //System.out.println(class_Path);
+
 
         });
     }
 
-    private void saveToAllBlocksAppToFile(List<String> listOfPath, String directoryOfApp, String AppName){
-        listOfPath.forEach(y -> {
-            System.out.println("Path: " + y);
+    private void saveAllBlocksAppToFile(List<String> listOfPath, String directoryOfApp, String AppName) {
+        listOfPath.forEach(nameWithPath -> {
+            int startOfPath = 0;
+            String name = nameWithPath;
+            if (nameWithPath.contains(AppName)) {
+                startOfPath = nameWithPath.lastIndexOf(AppName) + AppName.length();
+                name = nameWithPath.substring(nameWithPath.lastIndexOf(ParserClass.FILE_DELIMITER), nameWithPath.length());
+            }
+            String relativePath = nameWithPath.substring(startOfPath, nameWithPath.length() - name.length());
+            String relativePathWithName = nameWithPath.substring(startOfPath, nameWithPath.length());
 
-                int startOfPath = 0;
-                String name = y;
-                if(y.contains(AppName)){
-                    startOfPath = y.lastIndexOf(AppName) + AppName.length();
-                    name = y.substring(y.lastIndexOf(ParserClass.FILE_DELIMITER), y.length());
-                }
-                String relativePath = y.substring(startOfPath, y.length()-name.length());
-                String relativePathWithName = y.substring(startOfPath, y.length());
-                System.out.println("name "+ name);
-                System.out.println("relativePath "+ relativePath);
-                System.out.println("relativePathWithName "+ relativePathWithName);
-                //Path absFileName = Paths.get(directoryOfApp + ParserClass.FILE_DELIMITER + relativePathWithName);
-            System.out.println("-" + directoryOfApp+ParserClass.FILE_DELIMITER+ParserClass.BLOCK1_+relativePath);
-            saveFileToBlock(y, Paths.get(directoryOfApp+ParserClass.FILE_DELIMITER+ParserClass.BLOCK1_+relativePath),
-                    Paths.get(directoryOfApp+ParserClass.FILE_DELIMITER+ParserClass.BLOCK1_+relativePathWithName));
-
-            saveFileToBlock(y, Paths.get(directoryOfApp+ParserClass.FILE_DELIMITER+ParserClass.BLOCK2_+relativePath),
-                    Paths.get(directoryOfApp+ParserClass.FILE_DELIMITER+ParserClass.BLOCK2_+relativePathWithName));
-            saveFileToBlock(y, Paths.get(directoryOfApp+ParserClass.FILE_DELIMITER+ParserClass.BLOCK3_+relativePath),
-                    Paths.get(directoryOfApp+ParserClass.FILE_DELIMITER+ParserClass.BLOCK3_+relativePathWithName));
-
-
-
+            saveFileToBlock(AppName, name, nameWithPath, directoryOfApp, relativePath, relativePathWithName, ParserClass.BLOCK1_);
+            saveFileToBlock(AppName, name, nameWithPath, directoryOfApp, relativePath, relativePathWithName, ParserClass.BLOCK2_);
+            saveFileToBlock(AppName, name, nameWithPath, directoryOfApp, relativePath, relativePathWithName, ParserClass.BLOCK3_);
         });
 
     }
 
-    private void saveFileToBlock(String inputFilePath, Path directoryPath, Path fileNamePath){
+    private void saveFileToBlock(String AppName, String fileName, String inputFilePath, String directoryOfApp, String relativePath, String relativePathWithName, String block) {
+        String fileDelimiter = ParserClass.FILE_DELIMITER;
+        Path directoryPath = Paths.get(directoryOfApp + fileDelimiter + block + fileDelimiter + relativePath);
+        Path fileNamePath = Paths.get(directoryOfApp + fileDelimiter + block + relativePathWithName);
+
         Path file = Paths.get(inputFilePath);
         byte[] fileArray;
         try {
             fileArray = Files.readAllBytes(file);
             Files.createDirectories(directoryPath);
             Files.createFile(fileNamePath);
+            if (fileName.replace(ParserClass.FILE_DELIMITER, "").equals(ParserClass.PomXML)) {
+                fileArray = changeArtificialIdInPom(AppName, fileArray, block);
+            }
             Files.write(fileNamePath, fileArray);
 
         } catch (IOException e) {
@@ -132,20 +137,33 @@ public class FileHandlerParser implements IFileHandlerParser{
         }
     }
 
+    private byte[] changeArtificialIdInPom(String AppName, byte[] fileArray, String block) {
+        AppName = AppName.replace(ParserClass.FILE_DELIMITER, "");
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        try {
+            byteArrayOutputStream.write(fileArray);
+            String serverName = ParserClass.BLOCK_;
+            switch (block) {
+                case ParserClass.BLOCK1_:
+                    serverName += 1;
+                    break;
+                case ParserClass.BLOCK2_:
+                    serverName += 2;
+                    break;
+                case ParserClass.BLOCK3_:
+                    serverName += 3;
+                    break;
+            }
+            return byteArrayOutputStream.toString().replace("<artifactId>" + AppName, "<artifactId>" + AppName + serverName).getBytes();
+        } catch (IOException e) {
+            logger.error("error with modifying POM.XML file. ArtifactId remain unchanged.");
+            return fileArray;
+        }
+    }
 
-    private String getAppNameFromPath(String path){
+    private String getAppNameFromPath(String path) {
         String AppName = path.substring(0, path.indexOf(ParserClass.JAVA_SOURCE));
         AppName = AppName.substring(AppName.lastIndexOf(ParserClass.FILE_DELIMITER, AppName.length()));
         return AppName;
     }
-
-
-
-    public static void main(String[] args) {
-        FileHandlerParser fileHandlerParser = new FileHandlerParser(
-                "src/main/java/org/jboss/as/quickstarts/kitchensink/");
-       // fileHandlerParser.saveAppToFile();
-
-    }
-
 }
